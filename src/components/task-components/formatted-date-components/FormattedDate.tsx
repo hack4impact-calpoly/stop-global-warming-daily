02dbb5813927f77af1fb89d3a60549a0cedb74d0
Julian Labbe <jlabbe@calpoly.edu>
2026-01-26 15:29:59 -0800

    feat: Started building the FormattedDate components to be used in TaskCard

---
+++ src/components/task-components/formatted-date-components/FormattedDate.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Style from "./FormattedDate.module.css";
+
+interface FormattedDateProps {
+  date: Date;
+}
+
+/**
+    Author: Julian Labbe
+    Date: 1/26/26
+
+    Purpose: 
+        - FormattedDate formats the provided date to match the following form: DAY_STRING, DAY_NUMBER
+    @param date Date object to be formatted
+*/
+export default function FormattedDate({ date }: FormattedDateProps) {
+  const dayStringAbbr = date.toLocaleDateString("en-us");
+  const dayOfTheMonth = date.getDate();
+  return (
+    <div className={Style.formattedDateContainer}>
+      <p>{`${dayStringAbbr}, ${dayOfTheMonth}${calculateOrdinalSuffix(dayOfTheMonth)}`}</p>
+    </div>
+  );
+}
+
+/*
+    Author: Julian Labbe
+    Date: 1/26/26
+
+    Purpose: 
+        - Calculate the suffix to add onto the day number, based on the number of the month
+        - Will be one of the following: "st", "nd", "rd", "th" 
+        - Any values ending with digits 1, 2, or 3 will have one of the fist 3 options (exception for 11, 12 and 13)
+*/
+function calculateOrdinalSuffix(dayOfTheMonth: number) {}
--- src/components/task-components/task-card-components/TaskCard.tsx
+++ src/components/task-components/task-card-components/TaskCard.tsx
@@ -3,18 +3,24 @@ import Style from "./TaskCard.module.css";
 
 interface TaskCardProps {
   title: string;
-  date: Date;
+  date: Date | string;
   points: number;
   description: string;
 }
+
+/*
+    IMPORTANT:
+        - When dates are fetched from the database using JSON.stringify(), they will be converted into their string representations
+*/
 
 /**
     Author: Julian Labbe
     Date: 1/26/26
 
-    Purpose: TaskCard provides the layout/organzation for the individual components that make up the card's details
+    Purpose: 
+        - TaskCard provides the layout/organzation for the individual components that make up the card's details
     @param title Title for the specific task
-    @param date Date for the specific task
+    @param date Date or string for the specific task (see IMPORTANT comment block for more details)
     @param points The number of points the user will gain from completing the task
     @param description A brief description for the specific task
 */
