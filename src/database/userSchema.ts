083861fa0556d8bb8e132f29ff01b4a11d96f09c
danielerazo9 <derazo033@gmail.com>
2026-02-03 00:44:38 -0800

    feat: add user schema and enable mongo connection

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,9 +1,30 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 
-//! Example user schema. Not guaranteed to work
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+export enum Role {
+  user = "user",
+  admin = "admin",
+}
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export interface IUsers {
+  _id: Types.ObjectId;
+  email: string;
+  name: string;
+  role: Role;
+}
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, trim: true, lowercase: true },
+    name: { type: String, required: true, trim: true },
+    role: {
+      type: String,
+      enum: Object.values(Role),
+      required: true,
+      default: Role.user,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User ||
+  mongoose.model<IUsers>("User", UserSchema, "devusers");
