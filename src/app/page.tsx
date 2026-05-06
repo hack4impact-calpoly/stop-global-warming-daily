9ac1580e7331c0b6b572cabc1f4c5da6e7424510
Brady Welsh <blwelsh@calpoly.edu>
2026-05-05 22:15:00 -0700

    changes to hide page's contents until user status is known?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,6 +110,9 @@ export default function Home() {
     getChallenges();
   }, [userData?._id]);
 
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
   return (
     <main>
       <Box display={"flex"} justifyContent={"center"}>
